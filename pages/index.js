d02c530aa65bf80a2507700953f8d48a54d5cc01
Duyen Le <lengomyduyen@gmail.com>
2022-09-11 21:27:41 +0300

    Create home page

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,46 @@
 import { useEffect, useState, useRef, useContext } from 'react';
-import { Banner, CreatorCard } from '../components';
+import Image from 'next/image';
+import { useTheme } from 'next-themes';
+import { Banner, CreatorCard, NFTCard } from '../components';
 import { makeId } from '../utils/makeId';
 import images from '../assets';
 
 const Home = () => {
+  const { theme } = useTheme();
+  const [hideButtons, setHideButtons] = useState(false);
+
   const scrollRef = useRef(null);
   const parentRef = useRef(null);
+
+  // check if scrollRef container is overfilling its parentRef container
+  const isScrollable = () => {
+    const { current } = scrollRef;
+    const { current: parent } = parentRef;
+
+    if (current?.scrollWidth >= parent?.offsetWidth) return setHideButtons(false);
+    return setHideButtons(true);
+  };
+
+  const handleScroll = (direction) => {
+    const { current } = scrollRef;
+    const scrollAmount = window.innerWidth > 1800 ? 270 : 210;
+
+    if (direction === 'left') {
+      current.scrollLeft -= scrollAmount;
+    } else {
+      current.scrollLeft += scrollAmount;
+    }
+  };
+
+  // check if window is resized
+  useEffect(() => {
+    isScrollable();
+    window.addEventListener('resize', isScrollable);
+
+    return () => {
+      window.removeEventListener('resize', isScrollable);
+    };
+  });
 
   return (
     <div className="flex justify-center sm:px-4 p-12">
@@ -38,17 +73,43 @@ const Home = () => {
                   creatorEths={10 - i * 0.534}
                 />
               ))}
-              {/* {!hideButtons && (
-          <>
-            <div onClick={() => handleScroll('left')} className="absolute w-8 h-8 minlg:w-12 minlg:h-12 top-45 cursor-pointer left-0">
-              <Image src={images.left} layout="fill" objectFit="contain" alt="left_arrow" className={theme === 'light' ? 'filter invert' : undefined} />
+              {!hideButtons && (
+                <>
+                  <div onClick={() => handleScroll('left')} className="absolute w-8 h-8 minlg:w-12 minlg:h-12 top-45 cursor-pointer left-0">
+                    <Image src={images.left} layout="fill" objectFit="contain" alt="left_arrow" className={theme === 'light' ? 'filter invert' : undefined} />
+                  </div>
+                  <div onClick={() => handleScroll('right')} className="absolute w-8 h-8 minlg:w-12 minlg:h-12 top-45 cursor-pointer right-0">
+                    <Image src={images.right} layout="fill" objectFit="contain" alt="left_arrow" className={theme === 'light' ? 'filter invert' : undefined} />
+                  </div>
+                </>
+              )}
             </div>
-            <div onClick={() => handleScroll('right')} className="absolute w-8 h-8 minlg:w-12 minlg:h-12 top-45 cursor-pointer right-0">
-              <Image src={images.right} layout="fill" objectFit="contain" alt="left_arrow" className={theme === 'light' ? 'filter invert' : undefined} />
-            </div>
-          </>
-          )} */}
-            </div>
+          </div>
+        </div>
+
+        <div className="mt-10">
+          <div className="flexBetween mx-4 xs:mx-0 minlg:mx-8 sm:flex-col sm:items-start">
+            <h1 className="flex-1 font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold sm:mb-4">Hot Bids</h1>
+
+            {/* <div className="flex-2 sm:w-full flex flex-row sm:flex-col">
+              <SearchBar activeSelect={activeSelect} setActiveSelect={setActiveSelect} handleSearch={onHandleSearch} clearSearch={onClearSearch} />
+            </div> */}
+          </div>
+          <div className="mt-3 w-full flex flex-wrap justify-start md:justify-center">
+            {/* {nfts.map((nft) => <NFTCard key={nft.tokenId} nft={nft} />)} */}
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) => (
+              <NFTCard
+                key={`nft-${i}`}
+                nft={{
+                  i,
+                  name: `Nifty NFT ${i}`,
+                  price: (10 - i * 0.534).toFixed(2),
+                  seller: `0x${makeId(3)}...${makeId(4)}`,
+                  owner: `0x${makeId(3)}...${makeId(4)}`,
+                  description: 'Cool NFT on Sale',
+                }}
+              />
+            ))}
           </div>
         </div>
       </div>
