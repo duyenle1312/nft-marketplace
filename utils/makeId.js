529cde752b31c030aad5696003602fbec868e20f
Duyen Le <lengomyduyen@gmail.com>
2022-09-11 20:18:14 +0300

    Add creators list

--- utils/makeId.js
+++ utils/makeId.js
@@ -1,5 +1,12 @@
 export const makeId = (length) => {
-  const result = '';
-  const characters = 'sdf';
-  return characters;
+  let result = '';
+
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+
+  for (let i = 0; i < length; i += 1) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
 };
